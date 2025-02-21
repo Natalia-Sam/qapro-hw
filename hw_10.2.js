b1ce5194506738f79a099d39b5f617dc2bcf60ab
Natalia-Samarskaya <samarskaaya@gmail.com>
2025-02-21 14:40:36 +0200

    updated regexp

--- hw_10.2.js
+++ hw_10.2.js
@@ -6,12 +6,12 @@
 
 // Added space to the exceptions to avoid searching for matches between 2 different words (validation results like 'ful Jo', 'yful H')
 // Option 1 - searching with regExp without adding an additional variable
-const pattern = /[^Aa\s]{6}?/g;
+const pattern = /[^Aa\s]{6,}?/g;
 const str = "Wonderful Joyful Happiness Time Task Apple";
 console.log(str.match(pattern));
 
 // // Option 2 - searching with regExp wit adding an additional variable
-// const pattern = /[^Aa ]{6}?/g;
+// const pattern = /[^Aa ]{6,}?/g;
 // const str = "Wonderful Joyful Happiness Time Task Apple";
 // const matches = str.match(pattern);
 // console.log(matches);
